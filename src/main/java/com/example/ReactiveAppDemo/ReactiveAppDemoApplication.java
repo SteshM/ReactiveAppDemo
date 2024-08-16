44af3d351bb66ff6cdaf31a37e826ed30b3ae2a9
stella <mbithestella0@gmail.com>
2024-08-16 11:26:21 +0300

    Initial commit

---
+++ src/main/java/com/example/ReactiveAppDemo/ReactiveAppDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ReactiveAppDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveAppDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveAppDemoApplication.class, args);
+	}
+
+}
